1c21dca1d65dc5eae2c365ccce23351c1fe131f6
Daniel <dyankiver@gmail.com>
2021-01-07 07:23:39 -0500

    minor changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,22 +4,22 @@ class EventsController < ApplicationController
   require("bundler")
   Bundler.require()
 
-  def send_text(input)
-  # Your Account Sid and Auth Token from twilio.com/console
-  # and set the environment variables. See http://twil.io/secure
-  account_sid = 'ACb964b111bb91a4ac4ba8457804d27729'
-  auth_token = '1bf5ede4f3231a701777da909913ef96'
-  @client = Twilio::REST::Client.new(account_sid, auth_token)
-  
- 
-      message = @client.messages.create(
-          body: "#{input}",
-          from: '+12672144969',
-          to: '+19147557095'
-      )
-      puts message.sid
-  end 
-   
+    def send_text(input)
+        # Your Account Sid and Auth Token from twilio.com/console
+        # and set the environment variables. See http://twil.io/secure
+        account_sid = 'ACb964b111bb91a4ac4ba8457804d27729'
+        auth_token = '1bf5ede4f3231a701777da909913ef96'
+        @client = Twilio::REST::Client.new(account_sid, auth_token)
+        
+    
+        message = @client.messages.create(
+            body: "#{input}",
+            from: '+12672144969',
+            to: '+19147557095'
+        )
+        puts message.sid
+    end 
+    
     def index
         @events = @current_user.events 
     end 
@@ -32,13 +32,22 @@ class EventsController < ApplicationController
 
     def new 
        @event = Event.new
+
+    #    @user = User.find(params[:id])
+    #    @contact = Conctact.find(params[:id])
+
        @message = Message.new
     end 
 
     def create 
         @event = Event.create(event_params)
+        @user = User.find(params[:id])
+        # @contact = Conctact.find(params[:id])
+        byebug
         if @event.valid?
-            session[:event_id] = @event.id
+            # event_params[:user_id] = @user.id
+            # event_params[:contact_id] = @contact.id
+            event_params[:user_id] = @user.id
             redirect_to event_path(@event)
             send_text(event_params[:category])
         else 
